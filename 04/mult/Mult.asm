3423126142a2d5846f61d1cd398bc57a20dcbe00
MISUMI Masaru(deltam) <deltam@gmail.com>
2015-05-01 20:14:23 +0900

    make Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,21 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+    @2
+    M=0
+(LOOP)
+    @1
+    D=M
+    @END
+    D;JLE
+    @1
+    MD=D-1
+    @0
+    D=M
+    @2
+    M=D+M
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
