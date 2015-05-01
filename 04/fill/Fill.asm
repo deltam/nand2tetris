540288eea6ffb9c99a730fd1f7ba5732f138faaf
MISUMI Masaru(deltam) <deltam@gmail.com>
2015-05-01 21:42:00 +0900

    fill only

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,9 +3,33 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+(LOOP)
+    @KBD
+    D=M
+    @LOOP
+    D;JEQ
+    @24575 // SCREENの最後アドレス
+    M=-1   // 最後のアドレスに番兵を設定
+    @SCREEN
+    D=A
+    @R0
+    M=D
+(FILL)
+    @R0
+    A=M
+    D=M
+    @LOOP
+    D;JLT // SCREENメモリの番兵に到着したら終わる
+    @R0
+    A=M
+    M=-1
+    @R0
+    M=M+1
+    @FILL
+    0;JMP
